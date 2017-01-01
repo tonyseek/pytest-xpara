94843d714a9e9a2af691f0a634b805c907e251d3
Jiangge Zhang <tonyseek@gmail.com>
2017-01-02 00:37:44 +0800

    Add a prototype which support YAML parametrize

--- pytest_xpara/plugin.py
+++ pytest_xpara/plugin.py
@@ -1,1 +1,31 @@
 from __future__ import absolute_import
+
+import yaml
+import py.path
+
+
+def pytest_generate_tests(metafunc):
+    xpara_data = getattr(metafunc.module, '__xpara_data__', None)
+    if xpara_data is None:
+        current_dir = py.path.local(metafunc.module.__file__).dirpath()
+        xpara_data_name = metafunc.module.__name__.rsplit('.', 1)[-1]
+        xpara_data_file = current_dir.join('%s.yaml' % xpara_data_name)
+        if not xpara_data_file.exists():
+            return
+        xpara_data = yaml.load(xpara_data_file.read())
+        metafunc.module.__xpara_data__ = xpara_data
+
+    try:
+        mark = metafunc.function.parametrize_from_yaml
+    except AttributeError:
+        return
+    else:
+        xpara_data_name = (
+            mark.args[0] if mark.args else metafunc.function.__name__)
+
+    item = xpara_data.get(xpara_data_name)
+    if item:
+        item_args = item['args'].split(',')
+        item_data = [
+            [data.get(arg) for arg in item_args] for data in item['data']]
+        metafunc.parametrize(item['args'], item_data, ids=item.get('dataids'))
---
+++ tests/test_plugin.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+
+
+pytest_plugins = 'pytester'
+
+
+def test_run(testdir):
+    testdir.makefile('.yaml', test_foobar='''
+        test_foo:
+          args: foo,bar
+          data:
+            - foo: 13
+              bar: 15
+            - foo: 15
+              bar: 16
+
+        test_bar:
+          args: foo,bar
+          data:
+            - foo: 13
+              bar: 14
+            - foo: 15
+              bar: 16
+    ''')
+    testdir.makefile('.py', test_foobar='''
+        import pytest
+
+        @pytest.mark.parametrize_from_yaml
+        def test_foo(foo, bar):
+            assert foo + 2 == bar
+
+        @pytest.mark.parametrize_from_yaml
+        def test_bar(foo, bar):
+            assert foo + 1 == bar
+
+        def test_baz():
+            assert 1 + 1 == 2
+    ''')
+    result = testdir.runpytest_subprocess('--verbose')
+    result.assert_outcomes(passed=4, skipped=0, failed=1)
+    result.stdout.fnmatch_lines_random(r'''
+        test_foobar.py::test_foo*13-15* PASSED
+        test_foobar.py::test_foo*15-16* FAILED
+        test_foobar.py::test_bar*13-14* PASSED
+        test_foobar.py::test_bar*15-16* PASSED
+        test_foobar.py::test_baz PASSED
+    ''')
