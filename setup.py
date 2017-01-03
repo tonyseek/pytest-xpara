f3883a6536498fe0ce981a612a4ff1998e430fa8
Jiangge Zhang <tonyseek@gmail.com>
2017-01-03 19:44:07 +0800

    Update PyPI status to alpha

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
